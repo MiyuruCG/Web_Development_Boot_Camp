cf8c3d298bc76af786b594472836c36e12a32cf1
MiyuruCG <miyurugunatilake@gmail.com>
2020-06-29 15:06:46 +0530

    adding more routes to the server

--- 11.Express/my-express-server/server.js
+++ 11.Express/my-express-server/server.js
@@ -3,8 +3,24 @@ var express = require('express');
 const app = express();
 
 //sending an response to the browser
+//the get request from the browser to "/" <- home route
+//localhost:3000/
 app.get("/", function (req, res) {
     res.send("<h1>Hello World</h1>");
+});
+
+//localhost:3000/contact
+app.get("/contact", function (req, res) {
+    res.send("Contact me .... ");
+});
+
+//localhost:3000/about
+app.get("/about", function (req, res) {
+    res.send("My name is Miyuru and this is my first time using node express ");
+});
+
+app.get("/school", function (req, res) {
+    res.send("Schools..");
 });
 
 //listening in the port 3000

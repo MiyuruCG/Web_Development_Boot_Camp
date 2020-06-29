001d43bfef8b7c6af63cbb39f2c8bbeb81a7b59d
MiyuruCG <miyurugunatilake@gmail.com>
2020-06-29 12:04:37 +0530

    external pakage npm

--- 10. intro-to-node/index.js
+++ 10. intro-to-node/index.js
@@ -1,6 +1,16 @@
 //jshint esversion:6
 
+//internal node JS
+// const fs = require("fs");
 
-const fs = require("fs");
+// fs.copyFileSync("file1.txt", "file2.txt");
 
-fs.copyFileSync("file1.txt", "file2.txt");
+
+//external Node js
+
+var superheros = require('superheroes');
+
+var mySuperHeroName = superheros.random();
+
+console.log(mySuperHeroName);
+

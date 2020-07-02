95f7273340742990fc5f230cd50323c0330a386d
MiyuruCG <miyurugunatilake@gmail.com>
2020-07-02 22:14:33 +0530

    Get weather from a input city

--- 13.WeatherProject/app.js
+++ 13.WeatherProject/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const https = require("https");
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=91cda6351af38a63bafb2651f905659f&units=metric#"
+
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function (req, res) {
+
+    const query = req.body.cityName;
+    const apiKey = "91cda6351af38a63bafb2651f905659f";
+    const units = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + units + "#"
 
     https.get(url, function (response) {
         console.log(response.statusCode);
@@ -20,7 +31,7 @@ app.get("/", function (req, res) {
             console.log(temp);
             console.log(description);
             res.write("<p>The Weather is currently " + description + "</p>")
-            res.write("<h1>The temperature in london is : " + temp + " degrees Celcius</h1>");
+            res.write("<h1>The temperature in " + query + " is : " + temp + " degrees Celcius</h1>");
             res.write("<img src=" + imgUrl + ">");
 
             res.send();
@@ -29,10 +40,11 @@ app.get("/", function (req, res) {
 
     });
 
-    //res.send("server is up and running");
-})
+
+});
 
 
+//res.send("server is up and running");
 
 
 

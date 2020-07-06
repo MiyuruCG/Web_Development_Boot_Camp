8d8732bfd305d116a6c7eaae14d5da607b804100
MiyuruCG <miyurugunatilake@gmail.com>
2020-07-06 18:03:01 +0530

    adding CSS

--- 15.todolist-v1/app.js
+++ 15.todolist-v1/app.js
@@ -8,6 +8,7 @@ var items = ["Buy food", "Cook food"];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 
 app.get("/", function (reqS, res) {

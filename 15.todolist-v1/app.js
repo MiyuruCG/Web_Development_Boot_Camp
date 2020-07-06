807d9a9d667b19f458aa5f6d13c0fa5bf2e80545
MiyuruCG <miyurugunatilake@gmail.com>
2020-07-06 18:33:27 +0530

    adding teemplates in ejs

--- 15.todolist-v1/app.js
+++ 15.todolist-v1/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 var items = ["Buy food", "Cook food"];
+let workItems = [];
 //tells the app to use ejs as there view engine 
 app.set('view engine', 'ejs');
 
@@ -23,18 +24,36 @@ app.get("/", function (reqS, res) {
 
     var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", { kindOfDay: day, newListItems: items }); //look in the views folder for a file named "list"... then it will look for kindOfDay and assigns day.
+    res.render("list", { listTitle: day, newListItems: items }); //look in the views folder for a file named "list"... then it will look for kindOfDay and assigns day.
 });
 
 app.post("/", function (req, res) {
+    let item = req.body.newItem;
 
-    var item = req.body.newItem;
-    items.push(item);
-
-    res.redirect("/");
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
 
 });
 
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (res, req) {
+    let item = req.body.newItem;
+    workItems.push(item);
+
+    res.redirect("/work");
+})
+
+app.get("/about", function (req, res) {
+    res.render("about");
+});
 
 app.listen(3000, function (req, res) {
     console.log("Server connected on port 3000");

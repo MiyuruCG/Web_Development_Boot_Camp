d55e3ca46d6dcca52726487078c500631efa8a64
MiyuruCG <miyurugunatilake@gmail.com>
2020-07-07 16:47:53 +0530

    Challange 10 edit

--- 16.ejs-challenge/app.js
+++ 16.ejs-challenge/app.js
@@ -9,10 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-const post = {
-  title: "",
-  body: ""
-};
+
 
 app.set('view engine', 'ejs');
 
@@ -40,8 +37,10 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  post.title = req.body.titleTxt;
-  post.body = req.body.postTxt;
+  const post = {
+    title: req.body.titleTxt,
+    body: req.body.postTxt
+  };
 
   console.log(post);
 });

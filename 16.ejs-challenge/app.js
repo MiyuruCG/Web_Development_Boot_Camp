fecbf63a8805c0cbbae64b946512a24e8422a8d8
MiyuruCG <miyurugunatilake@gmail.com>
2020-07-07 11:53:07 +0530

    Challange 7

--- 16.ejs-challenge/app.js
+++ 16.ejs-challenge/app.js
@@ -31,7 +31,9 @@ app.get("/contact", function (req, res) {
   res.render("contact", { contactPage: contactContent });
 });
 
-
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
 
 
 app.listen(3000, function () {

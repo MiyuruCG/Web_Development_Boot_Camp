3fcdcbd25ccf385b44d92f94b35968c142265b52
MiyuruCG <miyurugunatilake@gmail.com>
2020-07-08 19:13:17 +0530

    Using mongoose to create and connect

--- 17.FruitsProject/app.js
+++ 17.FruitsProject/app.js
@@ -1,62 +1,141 @@
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+// const MongoClient = require('mongodb').MongoClient;
+// const assert = require('assert');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+const mongoose = require("mongoose");
 
-// Database Name
-const dbName = 'fruitsDB';
+//Creating and connecting to db using mongoose
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Create a new MongoClient
-const client = new MongoClient(url, { useUnifiedTopology: true });
-
-// Use connect method to connect to the Server
-client.connect(function (err) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-
-    const db = client.db(dbName);
-
-    findDocuments(db, function () {
-        client.close();
-    });
+//create new schema (Blueprints of the )
+const fruitSchema = new mongoose.Schema({
+    name: String,
+    rating: Number,
+    review: String
 });
 
-const insertDocuments = function (db, callback) {
-    // Get the documents collection
-    const collection = db.collection('fruits');
-    // Insert some documents
-    collection.insertMany([
-        {
-            name: "Mango",
-            rate: 5
-        },
-        {
-            name: "Banana",
-            rate: 5
-        },
-        {
-            name: "Oranges",
-            rate: 5
-        }
-    ], function (err, result) {
-        assert.equal(err, null);
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
-    });
-}
+//creates a new collection that follows the schema we defined above
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const findDocuments = function (db, callback) {
-    // Get the documents collection
-    const collection = db.collection('fruits');
-    // Find some documents
-    collection.find({}).toArray(function (err, fruits) {
-        assert.equal(err, null);
-        console.log("Found the following records");
-        console.log(fruits)
-        callback(fruits);
-    });
-}
+//create new fruit document
+const fruit = new Fruit({
+    name: "Apple",
+    rating: 7,
+    review: "good"
+});
+
+// //saves the fruit document inside the fruits Collection in the fruitDB
+// //fruit.save();
+
+
+//mongoose.connect("mongodb://localhost:27017/personDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+    name: "Jhon",
+    age: 37
+});
+
+person.save();
+
+const mango = new Fruit({
+    name: "mango",
+    rating: 7,
+    review: "good"
+});
+
+const orange = new Fruit({
+    name: "orange",
+    rating: 7,
+    review: "good"
+});
+
+Fruit.insertMany([mango, orange], function (err) {
+    if (err) {
+        console.log(err);
+
+    } else {
+        console.log("Saved success");
+
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//------------------------      Creating and connecting to db
+// // Connection URL
+// const url = 'mongodb://localhost:27017';
+
+// // Database Name
+// const dbName = 'fruitsDB';
+
+// // Create a new MongoClient
+// const client = new MongoClient(url, { useUnifiedTopology: true });
+
+// // Use connect method to connect to the Server
+// client.connect(function (err) {
+//     assert.equal(null, err);
+//     console.log("Connected successfully to server");
+
+//     const db = client.db(dbName);
+
+//     findDocuments(db, function () {
+//         client.close();
+//     });
+// });
+
+// const insertDocuments = function (db, callback) {
+//     // Get the documents collection
+//     const collection = db.collection('fruits');
+//     // Insert some documents
+//     collection.insertMany([
+//         {
+//             name: "Mango",
+//             rate: 5
+//         },
+//         {
+//             name: "Banana",
+//             rate: 5
+//         },
+//         {
+//             name: "Oranges",
+//             rate: 5
+//         }
+//     ], function (err, result) {
+//         assert.equal(err, null);
+//         assert.equal(3, result.result.n);
+//         assert.equal(3, result.ops.length);
+//         console.log("Inserted 3 documents into the collection");
+//         callback(result);
+//     });
+// }
+
+// const findDocuments = function (db, callback) {
+//     // Get the documents collection
+//     const collection = db.collection('fruits');
+//     // Find some documents
+//     collection.find({}).toArray(function (err, fruits) {
+//         assert.equal(err, null);
+//         console.log("Found the following records");
+//         console.log(fruits)
+//         callback(fruits);
+//     });
+// }

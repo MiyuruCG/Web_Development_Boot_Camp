4bcc710ff7f39fc8704828348b1b8c3cf550d178
MiyuruCG <miyurugunatilake@gmail.com>
2020-07-09 11:14:50 +0530

    setting up server v2

--- 18.Wiki-API/app.js
+++ 18.Wiki-API/app.js
@@ -2,26 +2,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const assert = require("assert");
 
 const app = express();
 
 app.set("view engine", ejs);
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true })); //parse our requests
+app.use(express.static("public")); // stores static files
 
 // Connection URL
-const url = 'mongodb://localhost:27017/wikiDB';
-mongoose.connect(url, { userNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
+//
 const articleSchema = {
     title: String,
     content: String
 };
-
+//article model
 const Article = mongoose.model("Article", articleSchema);
 
 
+
+//
 app.listen(3000, function () {
     console.log("Connected to port 3000");
 

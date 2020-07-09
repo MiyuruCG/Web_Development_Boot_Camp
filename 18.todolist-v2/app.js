6d4190c72d88f8b326ec755af193ac15d197d89d
MiyuruCG <miyurugunatilake@gmail.com>
2020-07-09 19:49:28 +0530

    shows the items in the db

--- 18.todolist-v2/app.js
+++ 18.todolist-v2/app.js
@@ -1,8 +1,6 @@
-//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -12,13 +10,52 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todoDB");
+mongoose.connect("mongodb://localhost:27017/todoListDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+//create schema
+const itemsSchema = {
+  name: String
+};
+
+//create mongoose model
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to your new ToDo List"
+});
+
+const item2 = new Item({
+  name: "Click + to add new item"
+});
+
+const item3 = new Item({
+  name: "<-- this to cross off the item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+
 
 app.get("/", function (req, res) {
 
-  const day = date.getDate();
-
-  res.render("list", { listTitle: day, newListItems: items });
+  Item.find({}, function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result.length === 0) {
+        Item.insertMany(defaultItems, function (err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Added the default items");
+          }
+        });
+        res.redirect("/");
+      } else {
+        res.render("list", { listTitle: "Today", newListItems: result });
+      }
+    }
+  });
 
 });
 
